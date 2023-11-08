eeec7c020665864cfffe60f7f991cd94edcb398b
Larsen-l <larsenlarsen777@gmail.com>
2023-11-08 11:24:02 +1100

    result out of 5

--- src/lib/results.svelte
+++ src/lib/results.svelte
@@ -2,10 +2,19 @@
 	import { goto } from "$app/navigation";
 
 	export let results: Result[];
+	let correctCount = 0;
+
+// Loop through the array and count the correct results
+for (const result of results) {
+  if (result.isCorrect === true) {
+    correctCount++;
+  }
+}
 </script>
 
 <div class="flex flex-col">
-	<h1 class="mx-auto font-bold">Results</h1>
+	<h1 class="mx-auto font-bold mb-0">Results</h1>
+	<h3 class="h3 mx-auto mb-[5rem]" >Final score {correctCount} of 5</h3>
 	{#each results as result}
 		<div class="flex justify-between space-x-6 m-4">
 			<p>{result.question}</p>
@@ -24,6 +33,6 @@
 <style>
 	h1 {
 		font-size: 2rem;
-		margin-bottom: 10rem;
+		margin-bottom: 2rem;
 	}
 </style>
