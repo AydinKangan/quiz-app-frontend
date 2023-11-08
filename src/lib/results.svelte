4c91962761ee38154cb256427cd2014415df9e6f
Aydin Elhan <200030355@student.kangan.edu.au>
2023-11-08 11:46:20 +1100

    added bold font

--- src/lib/results.svelte
+++ src/lib/results.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
+	import { goto } from '$app/navigation';
 
 	export let results: Result[];
 	let correctCount = 0;
 
-// Loop through the array and count the correct results
-for (const result of results) {
-  if (result.isCorrect === true) {
-    correctCount++;
-  }
-}
+	// Loop through the array and count the correct results
+	for (const result of results) {
+		if (result.isCorrect === true) {
+			correctCount++;
+		}
+	}
 </script>
 
 <div class="flex flex-col">
 	<h1 class="mx-auto font-bold mb-0">Results</h1>
-	<h3 class="h3 mx-auto mb-[5rem]" >Final score {correctCount} of 5</h3>
+	<h3 class="h3 mx-auto mb-[5rem] font-bold">Final score {correctCount} of 5</h3>
 	{#each results as result}
 		<div class="flex justify-between space-x-6 m-4">
 			<p>{result.question}</p>
@@ -26,7 +26,7 @@ for (const result of results) {
 		</div>
 	{/each}
 	<div class="flex justify-end mt-8">
-		<button on:click={() => goto("/dashboard")} class="btn variant-filled">Dashboard</button>
+		<button on:click={() => goto('/dashboard')} class="btn variant-filled">Dashboard</button>
 	</div>
 </div>
 
